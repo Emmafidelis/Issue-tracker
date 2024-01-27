d88bb73442869f79f8f0f053b0e07e7f34efa221
Emanuel Fidelis <emanuelkagombora28@gmail.com>
2024-01-27 20:35:52 +0300

    Fix caching issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -35,6 +35,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
       setSubmitting(true);
       if (issue) await axios.patch("/api/issues/" + issue.id, data);
       else await axios.post("/api/issues", data);
+      router.refresh();
       router.push("/issues");
     } catch (error) {
       setSubmitting(false);
