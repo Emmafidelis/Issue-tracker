d57feabf2a95c27ddd68ab5c1f77c128412e87a1
Emanuel Fidelis <emanuelkagombora28@gmail.com>
2024-02-09 19:58:48 +0300

    Add metadata

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -7,36 +7,48 @@ import DeleteIssueButton from "./DeleteIssueButton";
 import { getServerSession } from "next-auth";
 import authOptions from "@/app/auth/authOptions";
 import AssigneeSelect from "./AssigneeSelect";
+import { Metadata } from "next";
 
 interface Props {
-  params: { id: string };
+	params: { id: string };
 }
 
 const IssueDetailPage = async ({ params }: Props) => {
-  const session = await getServerSession(authOptions);
+	const session = await getServerSession(authOptions);
 
-  const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(params.id) },
-  });
+	const issue = await prisma.issue.findUnique({
+		where: { id: parseInt(params.id) },
+	});
 
-  if (!issue) notFound();
+	if (!issue) notFound();
 
-  return (
-    <Grid columns={{ initial: "1", sm: "5" }} gap="5">
-      <Box className="md:col-span-4">
-        <IssueDetails issue={issue} />
-      </Box>
-      {session && (
-        <Box>
-          <Flex direction="column" gap="3">
-            <AssigneeSelect issue={issue} />
-            <EditIssueButton issueId={issue.id} />
-            <DeleteIssueButton issueId={issue.id} />
-          </Flex>
-        </Box>
-      )}
-    </Grid>
-  );
+	return (
+		<Grid columns={{ initial: "1", sm: "5" }} gap="5">
+			<Box className="md:col-span-4">
+				<IssueDetails issue={issue} />
+			</Box>
+			{session && (
+				<Box>
+					<Flex direction="column" gap="3">
+						<AssigneeSelect issue={issue} />
+						<EditIssueButton issueId={issue.id} />
+						<DeleteIssueButton issueId={issue.id} />
+					</Flex>
+				</Box>
+			)}
+		</Grid>
+	);
 };
+
+export async function generateMetadata({ params }: Props) {
+	const issue = await prisma.issue.findUnique({
+		where: { id: parseInt(params.id) },
+	});
+
+	return {
+		title: issue?.title,
+		description: "Details of issue " + issue?.id,
+	};
+}
 
 export default IssueDetailPage;
